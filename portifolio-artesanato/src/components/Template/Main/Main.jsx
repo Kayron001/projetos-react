035c068df657b87ac57cda3c5e8a11de62f3f8f5
Kayron001 <101915317+Kayron001@users.noreply.github.com>
2022-08-04 13:05:04 -0300

    Update Main.jsx

--- portifolio-artesanato/src/components/Template/Main/Main.jsx
+++ portifolio-artesanato/src/components/Template/Main/Main.jsx
@@ -6,7 +6,7 @@ export default props =>
 <React.Fragment>
     <Header {...props}/>
     <main className="content container-fluid">
-        <div className='p-3 mt-3'>
+        <div className='main'>
             {props.children}
         </div>
     </main>
